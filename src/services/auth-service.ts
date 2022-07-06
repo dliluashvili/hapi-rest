662f7eb358f62892772800b2de3eb4dc6c2936dd
Davit Liluashvili <david.liluashvili7@gmail.com>
2022-07-06 04:53:49 +0400

    replaced native error with boom in auth-service.ts

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,4 +1,5 @@
 import { generateJwtToken } from './../utils/jwt'
+import Boom from '@hapi/boom'
 import { scrypt as _scrypt } from 'crypto'
 import { promisify } from 'util'
 import usersService from './users-service'
@@ -7,21 +8,17 @@ const scrypt = promisify(_scrypt)
 const signIn = async (username: string, password: string): Promise<string> => {
     const user = await usersService.findByUsername(username)
 
-    if (user) {
-        const [salt, storedHash] = user.password!.split('.')
+    const [salt, storedHash] = user.password!.split('.')
 
-        const hash = (await scrypt(password, salt, 32)) as Buffer
+    const hash = (await scrypt(password, salt, 32)) as Buffer
 
-        if (storedHash === hash.toString('hex')) {
-            const token = generateJwtToken(user._id?.toString()!)
+    if (storedHash === hash.toString('hex')) {
+        const token = generateJwtToken(user._id?.toString()!)
 
-            return token
-        }
-
-        throw new Error('User not found')
+        return token
     }
 
-    throw new Error('User not found')
+    throw Boom.badRequest('Incorrect credentials')
 }
 
 export default {
